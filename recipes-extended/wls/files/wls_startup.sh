02c1fbc4d339dda90af4db3d837efd6017ed4ba9
Armando Tello <armando.javier.mden@gmail.com>
2024-01-18 12:45:13 -0600

    fix: remove dash for sending parameters to initialization scripts

--- recipes-extended/wls/files/wls_startup.sh
+++ recipes-extended/wls/files/wls_startup.sh
@@ -8,9 +8,9 @@ cd /home/user/scripts
 ###########################################################
 #  Executions releated to hardware initialization processes 
 ###########################################################
-bash wls_hw_startup.sh -start
+bash wls_hw_startup.sh start
 
 ###########################################################
 #  Executions releated to software initialization processes 
 ###########################################################
-bash wls_sw_startup.sh -start
+bash wls_sw_startup.sh start
